95a1410ae5c69ffae5288ca4f4b95d74a222b1a0
bacrawu <bacrawu@tencent.com>
2023-07-20 02:53:05 +0800

    Init

---
+++ packages/isdiff/src/index.ts
@@ -0,0 +1,48 @@
+export function isdiff(lhs: any, rhs: any): boolean {
+  // 基础类型要求必须完全相同
+  // func 也必须要求完全一致
+  if (lhs === rhs) return true;
+
+  // 如果是undefined 或者null
+  // 上面又不相同，就直接返回false
+  if (!lhs || !rhs) return false;
+
+  // 后续的逻辑必须要求对象为object
+  const ltype = typeof lhs;
+  if (ltype !== 'object') return false;
+  if (ltype !== typeof rhs) return false;
+
+  if (Array.isArray(lhs)) {
+    if (!Array.isArray(rhs)) return false;
+    if (lhs.length !== rhs.length) return false;
+
+    // 不能使用every，[,2] 会直接跳过第一个元素的判断
+    // return lhs.every((item, index) => isdiff(item, rhs[index]));
+
+    for (let i = lhs.length; i--;) {
+      if (!isdiff(lhs[i], rhs[i])) return false;
+    }
+    return true;
+  }
+
+  if (Array.isArray(rhs)) return false;
+
+  // const lkeys = Object.keys(lhs);
+  // const rkeys = Object.keys(rhs);
+
+  // 不能直接判断keys的长度，需要考虑undefined
+  // if (lkeys.length !== rkeys.length) return false;
+
+  const hasCheckedKeys: { [key: string]: true } = Object.create(null);
+  return Object.keys(lhs).every((key) => {
+    const ret = isdiff(lhs[key], rhs[key]);
+    if (ret) hasCheckedKeys[key] = true;
+    return ret;
+  })
+    && Object.keys(rhs).every((key) => {
+      if (hasCheckedKeys[key] !== true) {
+        return isdiff(lhs[key], rhs[key]);
+      }
+      return true;
+    });
+}
---
+++ packages/isdiff/test/test_base.js
@@ -0,0 +1,70 @@
+const { isdiff } = require('../');
+const expect = require('expect.js');
+
+describe('#base', () => {
+  it('#basetype', () => {
+    expect(isdiff(1, 1)).to.be(true);
+    expect(isdiff('abc', 'abc')).to.be(true);
+    expect(isdiff(true, true)).to.be(true);
+    expect(isdiff(null, null)).to.be(true);
+    expect(isdiff(undefined, undefined)).to.be(true);
+
+    expect(isdiff(null, undefined)).to.be(false);
+    expect(isdiff(null, NaN)).to.be(false);
+    expect(isdiff(undefined, NaN)).to.be(false);
+    expect(isdiff(NaN, NaN)).to.be(false);
+    expect(isdiff(NaN, 0)).to.be(false);
+  });
+
+  it('#object', () => {
+    expect(isdiff({}, {})).to.be(true);
+    expect(isdiff(
+      { a: 1, b: 'b', c: { d: 1 } },
+      { a: 1, b: 'b', c: { d: 1 } }
+    )).to.be(true);
+
+    expect(isdiff({}, [])).to.be(false);
+    expect(isdiff({}, undefined)).to.be(false);
+    expect(isdiff({}, null)).to.be(false);
+
+    expect(isdiff(
+      { a: 1, b: 'b', c: { d: 1 } },
+      { a: 1, b: 'b' }
+    )).to.be(false);
+
+    expect(isdiff(
+      { a: 1, b: 'b' },
+      { a: 1, b: 'b', c: { d: 1 } }
+    )).to.be(false);
+
+
+    // 不支持symbol的对比
+    const symbol = Symbol('abc');
+    expect(isdiff({ [symbol]: 1 }, { [symbol]: 2 })).to.be(true);
+    expect(isdiff({}, { [symbol]: 1 })).to.be(true);
+    expect(isdiff(
+      Object.defineProperty({}, symbol, { enumerable: true, value: 1 }),
+      Object.defineProperty({}, symbol, { enumerable: true, value: 2 })
+    )).to.be(true);
+  });
+
+  it('#array', () => {
+    expect(isdiff([], [])).to.be(true);
+    expect(isdiff([1, {}], [1, {}])).to.be(true);
+    expect(isdiff([, 2], [, 2])).to.be(true);
+    expect(isdiff([, 2], [undefined, 2])).to.be(true);
+
+    expect(isdiff([1, 2], [1])).to.be(false);
+    expect(isdiff([1], [1, 2])).to.be(false);
+
+    debugger;
+    expect(isdiff([, 2], [1, 2])).to.be(false);
+  });
+
+  it('#function', () => {
+    expect(isdiff(function() {}, function() {})).to.be(false);
+
+    const func = function() {}
+    expect(isdiff(func, func)).to.be(true);
+  });
+});
